41a8c593bba241c6ea9eecd08bee0b152457813d
sarony <saron.yitbarek@gmail.com>
2014-02-15 21:52:49 -0500

    made post_types, tied to posts and on new form

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216005717) do
+ActiveRecord::Schema.define(version: 20140216024153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "post_types", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.text     "title"
@@ -22,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140216005717) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "body"
+    t.string   "post_type"
   end
 
 end
